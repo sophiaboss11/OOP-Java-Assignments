36d0166772910c015ec36ab8b42602a43c42a73b
sophiaboss11 <51395437+sophiaboss11@users.noreply.github.com>
2020-07-18 19:19:44 -0700

    midterm practive

---
+++ Midterm_practice.java
@@ -0,0 +1,40 @@
+//exam 1 study guide
+
+
+//6
+public static boolean midPoint( int a , int b , int c ){
+	 // find  a + b / 2
+
+	if (a+b/2=c || a+c/2=b || b+c/2=a){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+//4 after 6 	
+	//parameter is limit(while sum <= parameter)  declare sum
+	// round to decimal point
+	//sum = sum + 1/i ;
+
+
+public static double sequenceLimit(double limit){
+	double sum = 1.0;
+	System.out.print ("1 +");
+
+	while (sum <= limit){
+		sum = sum + (1 / i) ;
+		if(i>1){
+			System.out.print("1/" + i + "+");
+		}
+
+	}
+	System.out.print("="+sum);
+	return sum;
+
+}
+
+
+//
+
